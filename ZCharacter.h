d51d58fd56a8337aad05f5e21cb5ab85516e8da5
HoaNgocTram <92615754+HoaNgocTram@users.noreply.github.com>
2023-08-28 03:49:13 +0700

    Create ZCharacter.h

---
+++ ZCharacter.h
@@ -0,0 +1,18 @@
+//find
+//struct ZCharacterStatus
+//{
+//add
+MMatchWeaponType LastWeapon;
+
+//find
+//ZCharacterStatus() :
+//add inside
+LastWeapon(MWT_NONE),
+
+//find
+//void SetAdminHide(bool bHide) { m_dwStatusBitPackingValue.Ref().m_bAdminHide = bHide; }
+//add below
+MMatchWeaponType WType;
+void SetWeaponDamaged(MMatchWeaponType wWeapon) { WType = wWeapon; }
+MMatchWeaponType GetWeaponDamaged() { return WType; }
+MMatchWeaponType GetLastW() { return GetStatus().Ref().LastWeapon; }
