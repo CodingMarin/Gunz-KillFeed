a929e19bde8c55961484b88bfcb60cfe512bae12
HoaNgocTram <92615754+HoaNgocTram@users.noreply.github.com>
2023-08-28 04:43:40 +0700

    Create ZWeapon.cpp

---
+++ ZWeapon.cpp
@@ -0,0 +1,11 @@
+//find
+//void ZWeaponRocket::Explosion()
+//{
+//add inside {
+ZPostLastWeaponUsed(MWT_ROCKET);
+
+//find
+//void ZWeaponItemKit::Explosion()
+//{
+//add inside {
+ZPostLastWeaponUsed(MWT_FRAGMENTATION);
